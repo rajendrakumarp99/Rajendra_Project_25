b8b122d6544edbf559ff41e107371c8f850311a2
Rajendrakumar p <rajendrakumarp99@gmail.com>
2025-01-08 09:53:34 +0530

    Varibles added

--- src/main/java/rajendra_25/demo/DemoApplication.java
+++ src/main/java/rajendra_25/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+int i=20;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
