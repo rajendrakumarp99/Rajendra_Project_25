1794822210252b88cfc2b5563ed79da110bb8073
Rajendrakumar p <rajendrakumarp99@gmail.com>
2025-01-08 21:47:02 +0530

    Varibale Added

--- src/main/java/rajendra_25/demo/DemoApplication.java
+++ src/main/java/rajendra_25/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 int i=20;
+int j=23;
+int x=333;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
